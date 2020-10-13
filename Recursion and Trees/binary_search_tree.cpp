4dff23f16cd2a61191f3ca02ad32eb2e3aa8b024
exismys <riteshzeronity@gmail.com>
2020-10-13 08:58:41 +0530

    updated binary trees

--- Recursion and Trees/binary_search_tree.cpp
+++ Recursion and Trees/binary_search_tree.cpp
@@ -74,6 +74,72 @@ void insert(int item){
 	}
 }
 
+// delete if there is no or one child node
+void del1(Node* n, Node* par){
+	Node* child = NULL;
+	if (n->left == NULL && n->right == NULL){
+		child = NULL;
+	}
+	else if (n->left != NULL){
+		child = n->left;
+	}
+	else{
+		child = n->right;
+	}
+	if (par != NULL){
+		if (n == par->left){
+			par->left = child;
+		}
+		else{
+			par->right = child;
+		}
+	}
+	else{
+		root = child;
+	}
+}
+
+// delete if there are two child nodes
+void del2(Node* n, Node* par){
+	Node* suc = NULL;
+	Node* parsuc = NULL;
+	Node* save = NULL;
+	Node* ptr = n->right;
+	while (ptr->left != NULL){
+		save = ptr;
+		ptr = ptr->left;
+	}
+	suc = ptr;
+	parsuc = save;
+	del1(suc, parsuc);
+	if (par != NULL){
+		if (n == par->left){
+			par->left = suc;
+		}
+		else{
+			par->right = suc;
+		}
+	}
+	else{
+		root = suc;
+	}
+	suc->left = n->left;
+	suc->right = n->right;
+}
+
+void del(int item){
+	Node* status = search(item);
+	if (status == NULL){
+		cout << "Didn't find the item!" << endl;
+	}
+	else if (status->left != NULL && status->right != NULL){
+		del2(status, parent);
+	}
+	else{
+		del1(status, parent);
+	}
+}
+
 void preOrder(Node* root){
 	if (root != NULL){
 		cout << root->data << endl;
@@ -105,7 +171,8 @@ int main(){
 		cout << "2. preOrder traversal" << endl;
 		cout << "3. inOrder traversal" << endl;
 		cout << "4. postOrder traversal" << endl;
-		cout << "5. To quit the program" << endl;
+		cout << "5. To delete an item" << endl;
+		cout << "6. To quit the program" << endl;
 		cout << "Enter: ";
 		cin >> option;
 		if (option == 1){
@@ -124,6 +191,12 @@ int main(){
 			postOrder(root);
 		}
 		else if (option == 5){
+			int item;
+			cout << "Enter the item to delete: ";
+			cin >> item;
+			del(item);
+		}
+		else if (option == 6){
 			break;
 		}
 		else{
